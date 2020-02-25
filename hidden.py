4120df3189a3c476d6995fac9ac0104cdf2926ae
lero4kaa <57793106+lero4kaa@users.noreply.github.com>
2020-02-25 10:15:35 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "wD0ffDg0nQHvlIbZdw4531g1L",
-            "consumer_secret": "ef696IA644rcStHui1O4H3rdglncVaQu6MvMzeLpUocGhjvd8u",
-            "token_key": "1229876230637473792-CPJF1WY7Ji1KmefBlUagb9AMaoYk24",
-            "token_secret": "t2SekbDyqB2QkQ2E8l2NmRrtnTPL6h7j7qReevIHz2GQa"}
+    return {"consumer_key": "put your consumer_key here",
+            "consumer_secret": "put your consumer_secret here",
+            "token_key": "put your token_key here",
+            "token_secret": "put your token_secret here"}
